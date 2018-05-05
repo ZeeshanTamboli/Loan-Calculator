4da0eea4785eff440af1ad24fd451312b748f3ab
Zeeshan Tamboli <zeeshan.tamboli@gmail.com>
2018-05-05 23:27:51 +0530

    Optimized the calculateResults() function and added error

--- app.js
+++ app.js
@@ -14,21 +14,51 @@ function calculateResults(e) {
   const totalInterest = document.querySelector('#total-interest');
 
   //MONTHLY PAYMENT
-  const monthlyInterestRate = interest.value / 100 / 12;
-  const noOfMonths = 12 * years.value;
+  const principal = parseFloat(amount.value);
+  const monthlyInterestRate = parseFloat(interest.value) / 100 / 12;
+  const noOfMonths = 12 * parseFloat(years.value);
 
   const calculateMonthlyPayment =
-    amount.value *
+    principal *
     monthlyInterestRate /
     (1 - Math.pow(1 + monthlyInterestRate, -noOfMonths));
 
-  monthlyPayment.value = +calculateMonthlyPayment.toFixed(2); //toFixed() returns a string. '+' operator is used to convert string to number.
+  if (isFinite(calculateMonthlyPayment)) {
+    monthlyPayment.value = +calculateMonthlyPayment.toFixed(2); //toFixed() returns a string. '+' operator is used to convert string to number.
 
-  // TOTAL PAYMENT
-  totalPayment.value = Math.round(monthlyPayment.value * noOfMonths);
+    // TOTAL PAYMENT
+    totalPayment.value = Math.round(monthlyPayment.value * noOfMonths);
 
-  //TOTAL INTEREST
-  totalInterest.value = Math.round(totalPayment.value - amount.value);
+    //TOTAL INTEREST
+    totalInterest.value = Math.round(totalPayment.value - principal);
+  } else {
+    showError('Please check your numbers.');
+  }
 
   e.preventDefault();
+}
+
+function showError(error) {
+  //Create a div element
+  const errorDiv = document.createElement('div');
+
+  //Get elements
+  const card = document.querySelector('.card');
+  const heading = document.querySelector('.heading');
+
+  //Add bootstrap classes
+  errorDiv.className = 'alert alert-danger';
+
+  //Add text node
+  errorDiv.appendChild(document.createTextNode(error));
+
+  //Display errorDiv in DOM
+  card.insertBefore(errorDiv, heading);
+
+  setTimeout(clearError, 3000);
+}
+
+//Clear Error function
+function clearError() {
+  document.querySelector('.alert').remove();
 }
